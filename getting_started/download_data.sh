6328f7c0e9400ae4ddba49a0b6cca327ff03dbe7
Robert Crowe <robertcrowe@google.com>
2020-08-02 15:09:49 -0700

    Update download_data.sh
    Removed directory check for usage in Colab, and added dataset attribution.

--- getting_started/download_data.sh
+++ getting_started/download_data.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
+# The YouTube Spam Collection has been created by Tiago A. Almeida and Tulio C. Alberto.
+# http://www.dcomp.sor.ufscar.br/talmeida/
+# https://github.com/tuliocasagrande
+# See http://dcomp.sor.ufscar.br/talmeida/youtubespamcollection/
+
 FILES=( "Youtube01-Psy.csv" "Youtube02-KatyPerry.csv" "Youtube03-LMFAO.csv" "Youtube04-Eminem.csv" "Youtube05-Shakira.csv" )
 DATA_URL="http://archive.ics.uci.edu/ml/machine-learning-databases/00380/YouTube-Spam-Collection-v1.zip"
 RELOAD=false
